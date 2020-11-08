c19e0db854428ec275e70952105152a6318de651
Whackamoley109 <peteryushing@gmail.com>
2020-11-08 18:59:15 -0500

    website

---
+++ nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-custom">
+ <a class="navbar-brand" href="index.php">
+     <img src="handlogo.png" alt="Logo" width="30" height="30">
+ </a>
+ 
+  <a class="navbar-brand" href="index.php">SHS Hand</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+    <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="signup.php">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+    </ul>
+
+  </div>
+</nav>
